1a21c24cb49af06957ee4af914ba72c12dd5797c
sl2902 <laxmsun@hotmail.com>
2026-02-11 18:54:29 +0530

    add new config variables

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
+model_name = "google/medgemma-4b-it"
 
 scenarios = ["uri", "htn_followup", "t2dm", "back_pain", "annual_physical"]
+personas = ["brooks", "chen"]
 
 base_dir = "/kaggle/input/prompts/"
+gold_prompt_dir = "/kaggle/input/gold-prompts/generated_prompts"
+persona_prompt_dir = "/kaggle/input/personas"
+persona_generated_dir = "/kaggle/input/personas-generated/generated_personas"
